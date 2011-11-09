a2c52bf252a13aa14ded4a30e0dd83a12e7babec
Dan True <Dan True@DanTrue-brbar>
2011-11-09 17:13:49 +0100

    Percepts from world

--- src/worldmodel/MapAgent.java
+++ src/worldmodel/MapAgent.java
@@ -11,12 +11,19 @@ import java.awt.Color;
  * @author jakobenvy
  */
 public class MapAgent extends MapObject {
+    int number;
+    
     public MapAgent(long position){
         super(position);
         setBackground(Color.RED);
     }
-    public MapAgent(int x, int y){
+    public MapAgent(int num, int x, int y){
         super(x,y);
         setBackground(Color.RED);
+        this.number = num;
+    }
+    
+    public int getNumber() {
+        return this.number;
     }
 }
