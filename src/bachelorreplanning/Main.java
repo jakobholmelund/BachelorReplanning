52b9c678b7734f9cbd91ea0df1de812c0e1c0630
Dan True <Dan True@DanTrue-brbar>
2011-11-23 12:38:51 +0100

    Removed forward package. Renamed naiveplanning package

--- src/bachelorreplanning/Main.java
+++ src/bachelorreplanning/Main.java
@@ -4,18 +4,10 @@
  */
 package bachelorreplanning;
 
-import Planner.Logic;
 import Planner.forward.FSPlanner;
-import Planner.forward.Plan;
-import Planner.forward.Problem;
-import Planner.forward.State;
-import controllers.AddItemController;
 import gui.MainWindow;
 import jTrolog.errors.PrologException;
-import jTrolog.parser.Parser;
-import jTrolog.engine.*;
-import jTrolog.terms.*;
-import worldmodel.Box;
+import worldmodel.MapBox;
 import worldmodel.Goal;
 import worldmodel.MapAgent;
 import worldmodel.Wall;
@@ -31,19 +23,63 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws PrologException, Throwable {
-        World world = new LargeWorld();
+        World world = new World(10,10);
+        world.addObject(new MapAgent(0, 1 ,1));
+        world.addObject(new Wall(0,0));
+        world.addObject(new Wall(1,0));
+        world.addObject(new Wall(2,0));
+        world.addObject(new Wall(3,0));
+        world.addObject(new Wall(4,0));
+        world.addObject(new Wall(5,0));
+        world.addObject(new Wall(6,0));
+        world.addObject(new Wall(7,0));
+        world.addObject(new Wall(8,0));
+        world.addObject(new Wall(9,0));
         
-        Box test = new Box("a",0,0);
-        AddItemController addItemController = new AddItemController(world);
-        addItemController.setActive(test);
-        MainWindow mainWindow = new MainWindow(addItemController);
+        world.addObject(new Wall(0,1));
+        world.addObject(new Wall(0,2));
+        world.addObject(new Wall(0,3));
+        world.addObject(new Wall(0,4));
+        world.addObject(new Wall(0,5));
+        world.addObject(new Wall(0,6));
+        world.addObject(new Wall(0,7));
+        world.addObject(new Wall(0,8));
+        world.addObject(new Wall(0,9));
+
+        world.addObject(new Wall(9,1));
+        world.addObject(new Wall(9,2));
+        world.addObject(new Wall(9,3));
+        world.addObject(new Wall(9,4));
+        world.addObject(new Wall(9,5));
+        world.addObject(new Wall(9,6));
+        world.addObject(new Wall(9,7));
+        world.addObject(new Wall(9,8));
+        world.addObject(new Wall(9,9));
+        
+        world.addObject(new Wall(0,9));
+        world.addObject(new Wall(1,9));
+        world.addObject(new Wall(2,9));
+        world.addObject(new Wall(3,9));
+        world.addObject(new Wall(4,9));
+        world.addObject(new Wall(5,9));
+        world.addObject(new Wall(6,9));
+        world.addObject(new Wall(7,9));
+        world.addObject(new Wall(8,9));
+        world.addObject(new Wall(9,9));
+        
+        world.addObject(new MapBox("a", 2,1));
+        world.addObject(new Goal("a",8,8));
+        
+        MainWindow mainWindow = new MainWindow();
         mainWindow.loadNewWorld(world);
         mainWindow.drawWorld();
+        mainWindow.drawOptions();
         FSPlanner agent = new FSPlanner(world);
         Thread init = new Thread(agent);
+        
         while(!agent.done()) {
             if(agent.iteration == 3) {
-                world.addObject(new Box("b", 5,8));
+                world.addObject(new MapBox("b", 5,8));
             }
             mainWindow.drawWorld();
             init.run();
