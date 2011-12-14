faa10fa4d4bedb211b414e8eec23475f4b1557b8
unknown <Dan True@.(none)>
2011-12-14 11:57:17 +0100

    Deleted a main class

--- src/bachelorreplanning/Main.java
+++ src/bachelorreplanning/Main.java
@@ -4,34 +4,43 @@
  */
 package bachelorreplanning;
 
+import Planner.backward.BSPlanner;
 import gui.MainWindow;
-import gui.RouteFinder.Astar;
 import jTrolog.errors.PrologException;
-import worldmodel.MapAgent;
 import worldmodel.World;
+import worlds.MiddleWorld;
+import worlds.SmallWorld;
 
 /**
  *
  * @author Dan True
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws PrologException, Throwable {
-        World world = new World(100,100); //new MiddleWorld();//new World(30,30);
-        world.addObject(new MapAgent(1,1,1));
-        //world.newAgentActionParse("moveAtomic(0,[5,10])");
-        //world.newAgentActionParse("pickUp(1,a)");
-        //world.newAgentActionParse("place(1,a)");
-        //World world = new World(30,30);
-        //world.addObject(new MapAgent(0,1,1));
-        //Astar test = new Astar();
-        //test.findPlan(world,"move(0,[28,28])");
-        //4294967301294967297L,4294967421554051613L
-        //MainWindow mainWindow = new MainWindow();
-        //mainWindow.loadNewWorld(world);
-       //mainWindow.drawOptions();
+    
+    public static void main(String[] args) throws PrologException, InterruptedException  {
+        World world = new MiddleWorld();//new World(30,30);
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.loadNewWorld(world);
+        BSPlanner planner = new BSPlanner(world, 0, "agentAt(0,[5;5])", null); // at(a,[5;5]) // agentAt(0,[5;5])
+        Thread init = new Thread(planner);
+        init.start();
+        //planner.run();//findAction(p, "boxAt(a,[5;5])");
+        /*
+            World world = new World(100,100); //new MiddleWorld();//new World(30,30);
+            world.addObject(new MapAgent(1,1,1));
+            //world.newAgentActionParse("moveAtomic(0,[5,10])");
+            //world.newAgentActionParse("pickUp(1,a)");
+            //world.newAgentActionParse("place(1,a)");
+            //World world = new World(30,30);
+            //world.addObject(new MapAgent(0,1,1));
+            //Astar test = new Astar();
+            //test.findPlan(world,"move(0,[28,28])");
+            //4294967301294967297L,4294967421554051613L
+            //MainWindow mainWindow = new MainWindow();
+            //mainWindow.loadNewWorld(world);
+           //mainWindow.drawOptions();
+         */
     }
 }
+
+
